1c491d163585478c9addd8eedbceaaa7f6a554b0
recreators-designs <recreaters.services@gmail.com>
2026-03-17 17:28:44 +0530

    inital commit

---
+++ backend/routers/adminroute.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const router = express.Router();
+import {adminLogin} from '../controllers/admincontroller.js';
+
+
+router.post('/login',adminLogin);
+
+export default router;
