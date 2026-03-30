ae8f9525203318f33f0e08567b33ca1e140f9c53
shilpa-kukreja <recreaters.services@gmail.com>
2026-03-30 17:57:04 +0530

    Update page.jsx

--- frontend/src/app/agents/statistics/page.jsx
+++ frontend/src/app/agents/statistics/page.jsx
@@ -142,7 +142,7 @@ export default function EricAIStatistics() {
             </motion.h2>
 
             <div className="mt-10 bg-white rounded-2xl p-10 border border-gray-200 shadow-sm">
-              <div className="grid grid-cols-3 gap-6">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
                 <div>
                   
                   <p className="text-xl text-[#0B5EA8] border border-gray-100 p-4 rouned-md shadow-xl">Faster Decisions</p>
